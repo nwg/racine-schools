6f77faf178d0adf6dc4d1d6902debe9e736551bc
Nathaniel Griswold <nategriswold@gmail.com>
2020-01-28 11:54:40 -0600

    add some new fields to spreadsheet / import

--- scripts/import_manual_data.py
+++ scripts/import_manual_data.py
@@ -32,6 +32,7 @@ def schools():
             d['is_elementary'] = get_bool(row['E'])
             d['is_middle'] = get_bool(row['M'])
             d['is_high'] = get_bool(row['H'])
+            d['is_private'] = get_bool(row['Private'])
 
             d['low_grade'] = format_grade(row['low grade'])
             d['high_grade'] = format_grade(row['high grade'])
@@ -39,6 +40,17 @@ def schools():
             d['state_school_id'] = row['School Code'] or None
             d['pss_ppin'] = row['PPIN'] or None
             d['nces_id'] = row['NCES'] or None
+            d['address1'] = row['Address1'] or None
+            d['address2'] = row['Address2'] or None
+            d['phone'] = row['Phone'] or None
+            d['website'] = row['Website'] or None
+            d['mission'] = row['Mission'] or None
+            d['report1'] = row['Report1'] or None
+            d['report2'] = row['Report2'] or None
+            d['logo'] = row['Logo']
+            d['disadvantaged_pct'] = row['Economically Disadvantaged'] or None
+            d['curriculum_focus'] = row['Curriculum Focus'] or None
+            d['num_students'] = row['Num Students'] or None
 
             yield d
 
