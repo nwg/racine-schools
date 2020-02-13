d744a15c46c955981effb27baa7ef3cb42bd6ae1
Nathaniel Griswold <nategriswold@gmail.com>
2020-02-13 09:45:38 -0600

    order schools by longname

--- app/db.py
+++ app/db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 from psycopg_utils import select, idequals
+from psycopg2 import sql
 
 conn = psycopg2.connect("dbname='schools' user='postgres' host='localhost' password=''")
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -11,6 +12,6 @@ def school_with_name(name):
     return cur.fetchone()
 
 def all_schools():
-    sql = select('schools', '*')
-    cur.execute(sql)
+    q = sql.SQL("""select * from schools order by longname""")
+    cur.execute(q)
     return cur.fetchall()
